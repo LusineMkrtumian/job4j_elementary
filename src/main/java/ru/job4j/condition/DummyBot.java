ce207f81493a832846396a2f97b8b1232ad144c5
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2022-10-20 09:58:00 +0300

    4.1.2. Глупый бот [#31323 #353110]

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -5,12 +5,11 @@ public class DummyBot {
     public static String answer(String question) {
         String rsl = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-            return "Hi, SmartAss.";
+            rsl = "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
-            return "See you later.";
-        } else {
-            return rsl;
+            rsl = "See you later.";
         }
+        return rsl;
     }
 
     public static void main(String[] args) {
