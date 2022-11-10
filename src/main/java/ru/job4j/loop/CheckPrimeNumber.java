1cba20a917cca8f8db36c319a79fe75e8aa02c58
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2022-11-10 15:55:26 +0300

    5.5. Простое число [#171694 #359673]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,7 +2,8 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = number == 1 ? true : false;
+        boolean prime;
+        prime = number == 1;
         for (int num = 2; num < number; num++) {
             if ((number % num) == 0) {
                 prime = false;
