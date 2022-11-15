81d71f060604b27c1a9d416d95dcfca00109dd3f
Мкртумян Лусине Самвеловна <l.mkrtumyan@1c-ksu.ru>
2022-11-15 13:59:43 +0300

    ForEachUsage

---
+++ src/main/java/ru/job4j/array/ForEachUsage.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class ForEachUsage {
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three", "four", "five"};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
